681de4ee526898dd10c50d72ad9f1587a4b11749
crossoverJie <crossoverjie@gmail.com>
2018-02-16 18:30:43 +0800

    :sparkles: Introducing new features.

--- netty-client/src/main/java/com/crossoverjie/netty/client/learning/handle/EchoClientHandle.java
+++ netty-client/src/main/java/com/crossoverjie/netty/client/learning/handle/EchoClientHandle.java
@@ -1,5 +1,12 @@
 package com.crossoverjie.netty.client.learning.handle;
 
+import io.netty.buffer.ByteBuf;
+import io.netty.buffer.Unpooled;
+import io.netty.channel.ChannelHandlerContext;
+import io.netty.channel.SimpleChannelInboundHandler;
+import io.netty.util.CharsetUtil;
+import org.slf4j.Logger;
+
 /**
  * Function:
  *
@@ -7,5 +14,29 @@ package com.crossoverjie.netty.client.learning.handle;
  *         Date: 16/02/2018 18:09
  * @since JDK 1.8
  */
-public class EchoClientHandle {
+public class EchoClientHandle extends SimpleChannelInboundHandler<ByteBuf> {
+
+    private final static Logger LOGGER = org.slf4j.LoggerFactory.getLogger(EchoClientHandle.class);
+
+    @Override
+    public void channelActive(ChannelHandlerContext ctx) throws Exception {
+
+        //客户端和服务端建立连接时调用
+        ctx.writeAndFlush(Unpooled.copiedBuffer("hello,netty", CharsetUtil.UTF_8)) ;
+    }
+
+    @Override
+    protected void channelRead0(ChannelHandlerContext channelHandlerContext, ByteBuf in) throws Exception {
+
+        //从服务端收到消息时被调用
+        LOGGER.info("客户端收到消息={}",in.toString(CharsetUtil.UTF_8)) ;
+
+    }
+
+    @Override
+    public void exceptionCaught(ChannelHandlerContext ctx, Throwable cause) throws Exception {
+        //异常时断开连接
+        cause.printStackTrace() ;
+        ctx.close() ;
+    }
 }
--- netty-client/src/main/java/com/crossoverjie/netty/client/learning/server/EchoClient.java
+++ netty-client/src/main/java/com/crossoverjie/netty/client/learning/server/EchoClient.java
@@ -1,5 +1,16 @@
 package com.crossoverjie.netty.client.learning.server;
 
+import com.crossoverjie.netty.client.learning.handle.EchoClientHandle;
+import io.netty.bootstrap.Bootstrap;
+import io.netty.channel.ChannelFuture;
+import io.netty.channel.ChannelInitializer;
+import io.netty.channel.EventLoopGroup;
+import io.netty.channel.nio.NioEventLoopGroup;
+import io.netty.channel.socket.SocketChannel;
+import io.netty.channel.socket.nio.NioSocketChannel;
+
+import java.net.InetSocketAddress;
+
 /**
  * Function:
  *
@@ -8,4 +19,39 @@ package com.crossoverjie.netty.client.learning.server;
  * @since JDK 1.8
  */
 public class EchoClient {
+
+    private final static String HOST = "127.0.0.1" ;
+    private final static int PORT = 11211 ;
+
+
+    public static void main(String[] args) throws InterruptedException {
+        new EchoClient().start();
+    }
+
+
+    private void start() throws InterruptedException {
+
+        EventLoopGroup group = new NioEventLoopGroup() ;
+
+        try {
+            Bootstrap bootstrap = new Bootstrap() ;
+            bootstrap.group(group)
+                    .channel(NioSocketChannel.class)
+                    .remoteAddress(new InetSocketAddress(HOST,PORT))
+                    .handler(new ChannelInitializer<SocketChannel>() {
+                        @Override
+                        protected void initChannel(SocketChannel socketChannel) throws Exception {
+                            socketChannel.pipeline().addLast(new EchoClientHandle());
+                        }
+                    });
+
+            //阻塞同步完成连接
+            ChannelFuture future = bootstrap.connect().sync() ;
+            future.channel().closeFuture().sync() ;
+
+        }finally {
+            //关闭线程池
+            group.shutdownGracefully().sync() ;
+        }
+    }
 }
